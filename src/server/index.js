83900236fe775df8bb2b197e85ba8d2f12dc7412
Al <xochipillitx@gmail.com>
2018-05-01 21:34:10 -0500

    Installed Mongoose, connected to MongoDB on mLab

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,9 @@ const compression = require('compression');
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const secrets = require('./secrets.js')
+const mongoose = require('mongoose')
+
 
 const app = express();
 
@@ -17,6 +20,26 @@ app.use(express.static(__dirname + './../../'))
 app.get('*', (req, res, next) => {
   res.sendFile(path.join(__dirname, '../client/index.html'));
 });
+
+// Mongoose - MongoDB connector
+const mongoDB = secrets.dbURL;
+// Set up default Mongoose Connection
+mongoose.connect(mongoDB).then(console.log("Establishing connection..."));
+// Get Mongoose to use global promise LIbrary
+mongoose.Promise = global.Promise;
+// Get default connection
+const db = mongoose.connection;
+// Connection and Error Handlers
+db.on('open', function(ref) {
+  console.log('Open connection to Mongo Server...');
+});
+db.on('connected', function(ref) {
+  console.log('Connected to Mongo Server.');
+})
+db.on('reconnect', function(ref) {
+  console.log('Reconnected to Mongo Server')
+})
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.listen(8000, () => {
   console.log(`Server running on port 8000.\nKeep "yarn wds" running in an other terminal.`)
